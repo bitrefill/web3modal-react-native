c94cf48bb70b3eb91a24ad7f90fb29568edeb498
Felipe Fabel <felipecamposfabel@gmail.com>
2023-06-15 16:18:56 -0300

    exposing getImageUrl

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import 'react-native-get-random-values';
 import '@walletconnect/react-native-compat';
 import '@ethersproject/shims';
 import './config/animations';
+import { ExplorerCtrl } from './controllers/ExplorerCtrl';
 
 export { Web3Modal } from './components/Web3Modal';
 export { Web3Button } from './components/Web3Button';
 export { useWeb3Modal } from './hooks/useWeb3Modal';
 export { IProvider, IProviderMetadata } from './types/coreTypes';
 export { useWalletConnect } from './hooks/useWalletConnect';
+
+export const getWalletImageUrl = ExplorerCtrl.getWalletImageUrl;
